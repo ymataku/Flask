bd399be59c35b7d811971328a476f9a2d29d4369
ymataku <s256282@wakayama-u.ac.jp>
2022-09-30 02:02:34 +0900

    make it better

--- app/app.py
+++ app/app.py
@@ -2,48 +2,31 @@ from flask import Flask, render_template,request
 import json
 import requests
 from function.janome import janome_kaiseki
-from function.wordcloud import create_wordcloud
-
-token = ""
+from function.wordcloud import create_wordcloud_svg
 
 app = Flask(__name__,static_folder='./static')
 
-def parse(x):
+def list_to_string(word_list):
     word = ""
-    for i in x:
+    for i in word_list:
         word = word +" "+ i
     return word
 
-@app.route('/') 
-def index():
-    janome = janome_kaiseki("すもももももももものうち")
-    return render_template('index.html',janome = janome)
-
-
 @app.route('/',methods=['GET'])
-def make():
+def cloud_get():
     return render_template('cloud.html')
 
 @app.route("/", methods=['POST'])
-def cloud():
+def cloud_post():
     res =  request.form['text']
     f = request.files.get('file')
     data = f.getvalue().decode("utf-8")
     word_list = janome_kaiseki(data)
-    word = parse(word_list)
-    tag_svg = create_wordcloud(word)
-    # test = type(test)
-    # if res == "":
-    #     res = data
-   
-
+    word = list_to_string(word_list)
+    tag_svg = create_wordcloud_svg(word)
+    if res == "":
+        res = data
     return render_template('cloud_result.html',tag = tag_svg)
-
-
-@app.route('/other')
-def other():
-    return 'Other World\n'
-  
 
 if __name__ == "__main__":
     app.run(debug=True)
